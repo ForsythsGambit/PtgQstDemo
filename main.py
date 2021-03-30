df2af70453e494ef8547b1844b04b5208284e8d0
ADVFRED <theadvfred@gmail.com>
2021-03-30 16:28:40 +0000

    Added SpcToTab to .gitignore for the nth time.

---
+++ main.py
@@ -0,0 +1,20 @@
+from PtgQst.eng import *
+import logger
+from data import *
+
+thaeron=piece("Thaeron Forsyth",0,0)
+data.piectoboard(thaeron)
+displog=True
+
+logger.register("main")
+def log(msg,lvl=0):
+    """wraper for the logger library"""
+    logger.inlog(msg=msg,lvl=lvl,src="main")
+    if displog==True:
+    	print(logger.outlog("main"))
+    else:
+        pass
+
+if __name__=="__main__":
+	while True:
+		pass
